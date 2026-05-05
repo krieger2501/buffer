ab292cab9299ae5b1b14bcd692daf46494845a77
Kevin Rieger <kevin.rieger@sap.com>
2026-05-05 14:00:37 +0200

    chore: load .env.local in drizzle config, ignore drizzle/meta in prettier

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,8 @@
 /// <reference types="node" />
 import { defineConfig } from 'drizzle-kit';
+import { config } from 'dotenv';
+
+config({ path: '.env.local' });
 
 export default defineConfig({
 	dialect: 'postgresql',
